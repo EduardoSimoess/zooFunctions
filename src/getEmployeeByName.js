79d4641ace48f63968033acd031278c51de075cf
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-05-20 16:12:03 -0300

    Quesito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return employees.find((employee) => employee.firstName === employeeName
+|| employee.lastName === employeeName);
 }
-
+console.log(getEmployeeByName('Wishart'));
 module.exports = getEmployeeByName;
