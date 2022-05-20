dfa2bb75deb765acff80ec34eed4b5b826008097
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-05-20 16:03:46 -0300

    Quesito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const {
+  species,
+} = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species.find((specie) => specie.name === animal)
+    .residents.every((wildAnimal) => wildAnimal.age >= age);
 }
-
+console.log(getAnimalsOlderThan('penguins', 10));
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -8,9 +8,6 @@ function getSpeciesByIds(...ids) {
     return [];
   }
   return ids.map((serialNumber) => species.find((specie) => specie.id === serialNumber));
-  // for (let index = 0; index < ids.length; index += 1) {
-  //   return species.find((specie) => specie.id === ids[index]);
-  // }
 }
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
