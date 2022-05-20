80c3d7c5966101aba49aa38331eebf0e5e80990f
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-05-20 15:44:23 -0300

    Quesito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,16 @@
+const {
+  species,
+} = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  }
+  return ids.map((serialNumber) => species.find((specie) => specie.id === serialNumber));
+  // for (let index = 0; index < ids.length; index += 1) {
+  //   return species.find((specie) => specie.id === ids[index]);
+  // }
 }
-
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
-// Dando inicio
