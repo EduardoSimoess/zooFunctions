04d2a7e6a4d87b79c6495d93ece0ea58fd8cfab7
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-05-20 21:45:14 -0300

    Quesito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const managed = employees.filter((employee) => employee.managers.includes(managerId));
+  return managed.map((led) => `${led.firstName} ${led.lastName}`);
 }
-
+console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
+// getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83')
 module.exports = { isManager, getRelatedEmployees };
