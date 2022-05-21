45679fdc5932c6e43a2ded5284dba5e5a58e4af1
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-05-21 13:30:45 -0300

    Quesito 9 pela metade

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,21 @@
+const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (!scheduleTarget) {
+    const days = Object.keys(hours);
+    const openingHours = Object.values(hours);
+    const obj = {};
+    days.forEach((day, index) => {
+      obj[day] = {
+        officeHour: `Open from ${openingHours[index].open}am until ${openingHours[index].close}pm`,
+        exhibition: 'olá',
+      };
+    });
+    return obj;
+  }
+  const desiredSpecie = species.find((specie) => specie.name === scheduleTarget);
+  return desiredSpecie.availability;
 }
-
+console.log(getSchedule());
 module.exports = getSchedule;
