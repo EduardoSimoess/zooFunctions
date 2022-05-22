e819b4513fa495b69e54d8f88f8aa0d901b798f4
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-05-22 13:07:33 -0300

    Quesito 11 - 2 primeiros testes aprovados

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,36 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const findInfoEmployee = (employeeName) => employees.find((employee) => employee.firstName
+=== employeeName || employee.lastName === employeeName);
 
+const findTakenCareNames = (ids) => ids.map((id) => (species.find((specie) => specie.id
+=== id)).name);
+
+const findTakenCareLocation = (ids) => ids.map((id) => (species.find((specie) => specie.id
+=== id)).location);
+
+const usingName = (name) => {
+  const employee = findInfoEmployee(name);
+  const employeesId = employee.id;
+  const employeeFullName = `${employee.firstName} ${employee.lastName}`;
+  const takenCareId = employee.responsibleFor;
+  const takenCareNames = findTakenCareNames(takenCareId);
+  const takenCareLocation = findTakenCareLocation(takenCareId);
+  const answer = {
+    id: employeesId,
+    fullName: employeeFullName,
+    species: takenCareNames,
+    locations: takenCareLocation,
+  };
+  return answer;
+};
+
+function getEmployeesCoverage(obj) {
+  const { name, id } = obj;
+  if (name) {
+    return usingName(name);
+  }
+}
+console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 module.exports = getEmployeesCoverage;
