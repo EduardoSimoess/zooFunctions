e1a2ff97a9096fcf3048fd6c514da8a085bd2030
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-05-22 09:49:05 -0300

    Quesito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,26 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const findEmplyee = (employeeId) => employees.find((employee) => employee.id === employeeId);
+const findAnimal = (animal) => species.find((specie) => specie.id === animal);
+const oldestAnimal = (residents) => residents.reduce((acc, resident, index) => {
+  if (index === 0) {
+    return resident;
+  }
+  if (resident.age > acc.age) {
+    return resident;
+  }
+  return acc;
+});
 
+function getOldestFromFirstSpecies(id) {
+  const employeeObject = findEmplyee(id);
+  const firstAnimalsId = employeeObject.responsibleFor[0];
+  const animalsArray = (findAnimal(firstAnimalsId)).residents;
+  const oldAnimal = oldestAnimal(animalsArray);
+  const { age, name, sex } = oldAnimal;
+  const array = [name, sex, age];
+  return array;
+}
+console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 module.exports = getOldestFromFirstSpecies;
