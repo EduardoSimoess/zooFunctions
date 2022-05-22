67e07b0af2fc1e31429715f247ba773bfe8bd094
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-05-22 16:56:48 -0300

    Quesitos 6 e 7

--- src/handlerElephants.js
+++ src/handlerElephants.js
@@ -32,5 +32,5 @@ const handlerElephants = (param) => {
   }
   return computeData(param, elephants);
 };
-
+console.log(handlerElephants('averageAge'));
 module.exports = handlerElephants;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,24 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Ao não receber nenhum parâmetro a funções retorna um valor "undefined"', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+
+  it('Ao receber um parâmetro diferente de uma string é retornada a frase "Parâmetro inválido, é necessário uma string"', () => {
+    expect(handlerElephants(1)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Ao receber um dos parâmetros deve ser retornado o parâmetro referente ao elefante', () => {
+    expect(handlerElephants('id')).toBe('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5');
+  });
+  it('Ao receber "count" é retornada a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Ao receber "names" é retornada um array com o npmde dos elefantes', () => {
+    const array = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+    expect(handlerElephants('names')).toEqual(array);
+  });
+  it('Ao receber "averageAge" é retornada a média da idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5, 5);
+  });
+});
