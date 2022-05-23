1de4e4926f72d5af07b343da91b552a983404598
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-05-23 18:58:22 -0300

    Quesitos 12, 13 e 16 correção no elint do 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,20 +1,20 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const locationArray = ['NE', 'NW', 'SE', 'SW'];
-const noArgument = () => {
-  const obj = {};
-  // const ne = species.filter((specie) => specie.location === locationArray[0]);
-  // const nw = species.filter((specie) => specie.location === locationArray[1]);
-  // const se = species.filter((specie) => specie.location === locationArray[2]);
-  // const sw = species.filter((specie) => specie.location === locationArray[3]);
-  const speciesNames = species.map((specie) => specie.name);
-  return locationArray.map((location) => species.filter((specie, index) => {
-    if (specie.location === location) {
-      return speciesNames[index];
-    }
-  }));
-};
+// const locationArray = ['NE', 'NW', 'SE', 'SW'];
+// const noArgument = () => {
+//   const obj = {};
+//   // const ne = species.filter((specie) => specie.location === locationArray[0]);
+//   // const nw = species.filter((specie) => specie.location === locationArray[1]);
+//   // const se = species.filter((specie) => specie.location === locationArray[2]);
+//   // const sw = species.filter((specie) => specie.location === locationArray[3]);
+//   const speciesNames = species.map((specie) => specie.name);
+//   return locationArray.map((location) => species.filter((specie, index) => {
+//     if (specie.location === location) {
+//       return speciesNames[index];
+//     }
+//   }));
+// };
 console.log(noArgument());
 function getAnimalMap(options) {
   if (!options) {
