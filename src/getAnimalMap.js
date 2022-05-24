1c1f3a0b8a6e1f3c35577c1358bbda4b25e1adbc
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-05-24 14:43:47 -0300

    Quesito 14 - Projeto finalizadogit add .git add .git add .

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -15,21 +15,42 @@ const noArgument = () => {
   return obj;
 };
 
-const findAnimalNames = (animalName) => {
-  const animalResidents = (species.find((animal) => animal.name === animalName)).residents;
-  return animalResidents.map((resident) => resident.name);
+const order = (truth, animalsName) => {
+  if (truth === true) {
+    return animalsName.sort();
+  }
+  return animalsName;
 };
-const createObj = (array) => array.map((animal) => {
+
+const findAnimalNames = (animalName, truth, gender) => {
+  const animalResidents = (species.find((animal) => animal.name === animalName)).residents;
+  if (!gender) {
+    const animalsName = animalResidents.map((resident) => resident.name);
+    return order(truth, animalsName);
+  }
+  if (gender === 'male') {
+    const maleAnimals = animalResidents.filter((resident) => resident.sex === gender);
+    const maleAnimalsNames = maleAnimals.map((animal) => animal.name);
+    return order(truth, maleAnimalsNames);
+  }
+  if (gender === 'female') {
+    const femaleAnimals = animalResidents.filter((resident) => resident.sex === gender);
+    const femaleAnimalsNames = femaleAnimals.map((animal) => animal.name);
+    return order(truth, femaleAnimalsNames);
+  }
+};
+const createObj = (array, truth, gender) => array.map((animal) => {
   const obj = {};
-  obj[animal] = findAnimalNames(animal);
+  obj[animal] = findAnimalNames(animal, truth, gender);
   return obj;
 });
 
-const names = (truth) => {
+const names = (truth, gender) => {
   const obj = {};
   const animals = Object.values(noArgument());
   const [NE, NW, SE, SW] = animals;
-  const array = [createObj(NE), createObj(NW), createObj(SE), createObj(SW)];
+  const array = [createObj(NE, truth, gender), createObj(NW, truth, gender),
+    createObj(SE, truth, gender), createObj(SW, truth, gender)];
   array.forEach((objetos, index) => {
     obj[locationArray[index]] = objetos;
   });
@@ -39,12 +60,15 @@ const names = (truth) => {
 function getAnimalMap(options) {
   if (!options) return noArgument();
 
-  const { includeNames } = options;
-  if (includeNames === true) return names();
-  // const { sort } = options;
-  // if (includeNames === true && sort === true)
+  const { includeNames, sorted, sex } = options;
+  if (includeNames === true) {
+    if (sex || sorted) {
+      return names(sorted, sex);
+    }
+    return names(false);
+  }
   return noArgument();
 }
-console.log(names());
+console.log(getAnimalMap({ includeNames: true }));
 
 module.exports = getAnimalMap;
